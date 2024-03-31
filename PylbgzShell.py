b69b61587a68a3cd56ac1f7771ee30eeb83ee41a
69gg <1708213363@qq.com>
2024-03-31 11:32:40 +0800

    v1.6.r-禁用pyc

--- PylbgzShell.py
+++ PylbgzShell.py
@@ -1,6 +1,9 @@
 import os
 import re
+
 import sys
+sys.dont_write_bytecode = True
+#禁用pyc
 
 from modules.compress import *
 from modules.decompress import *
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+sys.dont_write_bytecode = True
+#禁用pyc
+
 from modules.compress import *
 from modules.decompress import *
 from modules.lsfiles import *
